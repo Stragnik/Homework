eee7380d0118d0e4415a2401dcecbe2e9af9030f
Sosmirnov <Ssmirnovpro@gmail.com>
2021-03-29 19:08:19 +0300

    homework

---
+++ src/ru/geekbrains/homework/lesson_4/ui/Controller.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.homework.lesson_4.ui;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public TextArea chatTextArea;
+    @FXML
+    public TextField inputField;
+    @FXML
+    public Button sendButton;
+
+    public void sendMessageButton(ActionEvent actionEvent) {
+        chatTextArea.appendText("Вы: > " + inputField.getText() + "\n");
+        inputField.clear();
+        inputField.requestFocus();
+    }
+
+
+    public void pressEnterKey(ActionEvent actionEvent) {
+        inputField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    chatTextArea.appendText("Вы: > " + inputField.getText() + "\n");
+                    inputField.clear();
+                    inputField.requestFocus();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> inputField.requestFocus());
+    }
+}
